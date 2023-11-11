89e78481cef0615eee4505f51091d232e873f321
srijita patra <srijitapatra26092002@gmail.com>
2023-11-11 23:35:57 +0530

    frontend

---
+++ src/test/java/com/srijitapatra/thymeleafapp/ThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srijitapatra.thymeleafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
